8e7ec686f3181e9649d6f8bcd70d9d1270ffd64a
GeneChatham <genechatham@gmail.com>
2014-01-08 14:19:09 -0600

    Fix routes END error.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Ativa::Application.routes.draw do
   devise_for :users
   resources :posts
   root 'projects#index'
-  resources :project 
+  resources :project do
     resources :subscriptions, only: [:create, :destroy]
   end
 
   get 'users/:id' => 'users#show', as: 'user'
+
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -62,4 +64,4 @@ Ativa::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
